19f8455281ba0a2e2db5dfccf345310cc41d5f82
Sbyspaceopera <sebastiencorbisiermurumba@gmail.com>
2022-08-12 22:39:53 +0200

    page file started

--- page.php
+++ page.php
@@ -1,8 +1,13 @@
 <?php get_header() ?>
 
-<main id="atypic-main" class="px-40 ">
-    <h1><?php echo wp_title(); ?></h1>
-    <?php the_content() ?>
+<main id="atypic-main" class="flex py-5">
+    <div class="w-0 sm:visible sm:w-1/5">
+        <img class="-translate-x-1.5 w-full" src="<?php echo get_template_directory_uri() ?>/assets/images/Steren_Twisted_Branch_4.svg">
+    </div>
+    <div class="p-5 md:w-3/5"><?php the_content() ?></div>
+    <div class="w-0 md:visible sm:w-1/5">
+        <img class="translate-x-1.5 w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/assets/images/Steren_Twisted_Branch_4.svg">
+    </div>
 </main>
 
 <?php get_footer() ?>
