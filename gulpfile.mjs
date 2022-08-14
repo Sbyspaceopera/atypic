dc71fc0204193d2d4b5947f11401db49c084175f
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-14 14:01:28 +0200

    Handling esm import for gulp config and commonjs import for tailwind config since tailwind doesnt work with esm

---
+++ gulpfile.mjs
@@ -0,0 +1,27 @@
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import del from 'del';
+import imagemin from "gulp-imagemin";
+
+const sass = gulpSass(dartSass);
+
+gulp.task('styles', () => {
+    return gulp.src('assets/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('imgSquash', () => {
+    gulp.src('assets/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/images'))
+})
+
+gulp.task('watch', () => {
+    gulp.watch('assets/scss/*.scss', (done) => {
+        gulp.series(['styles'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['styles','imgSquash','watch']));
