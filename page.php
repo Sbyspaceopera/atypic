66ca07def4310bb802b714cdc2c5f929b45b9715
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-14 20:47:45 +0200

    page.php decorations and layout down

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php get_header() ?>
 
-<main id="atypic-main" class="flex py-5 px-1 bg-cover" >
+<main id="atypic-main" class="flex py-5 px-1" >
     <div class="atypic-main-left-laurel self-center w-0 sm:visible md:w-1/5">
-        <img class="w-full" src="<?php echo get_template_directory_uri() ?>/assets/images/laurel-single.svg">
+        <?php get_template_part('assets/svg/laurel', 'single-left.svg') ?>
     </div>
     <div class="p-5 md:w-3/5"><?php the_content() ?></div>
     <div class="atypic-main-right-laurel self-center w-0 md:visible md:w-1/5">
-        <img class="w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/assets/images/laurel-single-right.svg">
+        <?php get_template_part('assets/svg/laurel', 'single-right.svg') ?>
     </div>
 </main>
 
