e6f009b43969e4c1f7d42aa02378c5f2cbf9d886
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-14 14:09:31 +0200

    main_css

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php get_header() ?>
 
-<main id="atypic-main" class="flex py-5">
-    <div class="w-0 sm:visible sm:w-1/5">
-        <img class="-translate-x-1.5 w-full" src="<?php echo get_template_directory_uri() ?>/assets/images/Steren_Twisted_Branch_4.svg">
+<main id="atypic-main" class="flex py-5 px-1 bg-cover" >
+    <div class="atypic-main-left-laurel self-center w-0 sm:visible md:w-1/5">
+        <img class="w-full" src="<?php echo get_template_directory_uri() ?>/build/images/laurel-single.svg">
     </div>
     <div class="p-5 md:w-3/5"><?php the_content() ?></div>
-    <div class="w-0 md:visible sm:w-1/5">
-        <img class="translate-x-1.5 w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/assets/images/Steren_Twisted_Branch_4.svg">
+    <div class="atypic-main-right-laurel self-center w-0 md:visible md:w-1/5">
+        <img class="w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/build/images/laurel-single-right.svg">
     </div>
 </main>
 
