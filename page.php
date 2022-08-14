2338d02c786ac08d81ea4281141e00597742b277
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-14 14:19:40 +0200

    Let use none minified svg since they are empty after the the gulp build  process

--- page.php
+++ page.php
@@ -2,11 +2,11 @@
 
 <main id="atypic-main" class="flex py-5 px-1 bg-cover" >
     <div class="atypic-main-left-laurel self-center w-0 sm:visible md:w-1/5">
-        <img class="w-full" src="<?php echo get_template_directory_uri() ?>/build/images/laurel-single.svg">
+        <img class="w-full" src="<?php echo get_template_directory_uri() ?>/assets/images/laurel-single.svg">
     </div>
     <div class="p-5 md:w-3/5"><?php the_content() ?></div>
     <div class="atypic-main-right-laurel self-center w-0 md:visible md:w-1/5">
-        <img class="w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/build/images/laurel-single-right.svg">
+        <img class="w-full -scale-x-100" src="<?php echo get_template_directory_uri() ?>/assets/images/laurel-single-right.svg">
     </div>
 </main>
 
