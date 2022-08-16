9a2354c1037d9521e1dcee2a61b34829517c7105
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-16 21:12:19 +0200

    Footer.php done

--- footer.php
+++ footer.php
@@ -1,10 +1,17 @@
-<footer class="container bg-green-100">
+<footer class="atypic-footer w-full h-15 bg-black text-white flex justify-between p-3">
+    <div class="w-16 border-r-4 border-r-yellow-500 pr-5 flex items-center content-center justify-center">
+        <?php the_custom_logo(); ?>
+    </div>
     <?php
     wp_nav_menu(array(
         "theme_location" => "footer_menu",
         "container" => "nav"
     ));
     ?>
+    <div class="flex items-center content-center">
+        <a href="https://github.com/Sbyspaceopera"><img class="w-10 invert" src="<?php echo get_template_directory_uri() ?>/build/images/GitHub-Mark-120px-plus.png"></a>
+        <a href="https://www.linkedin.com/in/sebastiencorbisier/"><span class="dashicons dashicons-linkedin"></span></a>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
