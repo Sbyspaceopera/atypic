3db17b020204b5bdc39f34a5a04571b234ff70e9
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-16 19:19:59 +0200

    Minimal home.php: changing background images is need

--- home.php
+++ home.php
@@ -21,7 +21,7 @@
             ?>
                 <section class="w-full lg:grid grid-cols-3 grid-rows-2 min-h-[20rem] py-5">
                     <header class="min-h-[5rem] rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-bl-xl border-2 border-black w-full bg-no-repeat bg-center bg-cover col-span-1 row-span-2 p-1" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url(https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQyMjAtcGRmYW1vdXNwYWludGluZ2V0YzA4MzAwOS1pbWFnZV8zLmpwZw.jpg)">
-                        <h2 class="font-semibold underline <?php echo $textColors[$key] ?> text-3xl text-white text-center">
+                        <h2 class="font-semibold <?php echo $textColors[$key] ?> text-3xl text-white text-center">
                             <a href="<?php echo get_category_link($category->term_id) ?>"><?php echo $category->name ?></a>
                         </h2>
                         <p class="text-white text-xl"><?php echo $category->description ?></p>
@@ -35,7 +35,9 @@
                         ));
                         
                         foreach($posts as $post){ ?>
-                            <h3 class="underline <?php echo $decColors[$key] ?> text-2xl"><?php echo $post->post_title ?></h3>
+                            <h3 class="underline <?php echo $decColors[$key] ?> text-2xl">
+                                <a href="<?php echo get_permalink($post->ID) ?>"><?php echo $post->post_title ?></a>
+                            </h3>
                             <p>
                                 <?php echo wp_trim_words($post->post_content,35) ?>
                             </p>
