ddc0772231b1f788ab17e8f75a1438bd526520a7
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-17 17:58:42 +0200

    single.php done

--- single.php
+++ single.php
@@ -1,10 +1,21 @@
 <?php get_header() ?>
 
-<main id="atypic-main" class="flex py-5 px-1" >
+<main id="atypic-main" class="flex py-5 px-1">
     <div class="atypic-main-left-laurel self-center w-0 sm:visible md:w-1/5">
         <?php get_template_part('assets/svg/laurel', 'single-left.svg') ?>
     </div>
-    <div class="p-5 md:w-3/5"><?php the_content() ?></div>
+    <div class="p-5 md:w-3/5">
+        <h1 class="text-2xl text-center font-semibold">
+            <?php the_title();?>
+        </h1>
+        <div class="flex gap-2 py-1 text-black italic text-xs font-semibold flex-wrap justify-center">
+            <?php
+            $tags = get_the_tag_list('', '', '', $post->ID);
+            echo $tags;
+            ?>
+        </div>
+        <?php the_content() ?>
+    </div>
     <div class="atypic-main-right-laurel self-center w-0 md:visible md:w-1/5">
         <?php get_template_part('assets/svg/laurel', 'single-right.svg') ?>
     </div>
