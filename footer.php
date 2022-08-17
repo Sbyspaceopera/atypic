5460a6b0f965bda038ecc114915ee88f328d0799
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-17 17:56:14 +0200

    Footer done

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer class="atypic-footer w-full h-15 bg-black text-white flex justify-between p-3">
-    <div class="w-16 border-r-4 border-r-yellow-500 pr-5 flex items-center content-center justify-center">
+<footer class="atypic-footer w-full h-15 bg-black text-white flex justify-between p-3 felx-wrap">
+    <div class="w-[5rem] min-w-[4rem] border-r-4 border-r-yellow-500 pr-5 flex items-center content-center justify-center">
         <?php the_custom_logo(); ?>
     </div>
     <?php
@@ -8,8 +8,8 @@
         "container" => "nav"
     ));
     ?>
-    <div class="flex items-center content-center">
-        <a href="https://github.com/Sbyspaceopera"><img class="w-10 invert" src="<?php echo get_template_directory_uri() ?>/build/images/GitHub-Mark-120px-plus.png"></a>
+    <div class="flex items-center content-center flex-wrap gap-2">
+        <a href="https://github.com/Sbyspaceopera"><img class="w-[3rem] invert saturate-200" src="<?php echo get_template_directory_uri() ?>/build/images/GitHub-Mark-120px-plus.png"></a>
         <a href="https://www.linkedin.com/in/sebastiencorbisier/"><span class="dashicons dashicons-linkedin"></span></a>
     </div>
 </footer>
