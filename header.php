c7103b30b1637aa0656e4a9fc39805f678cfe567
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-23 20:48:18 +0200

    Small aesthetic update

--- header.php
+++ header.php
@@ -12,10 +12,10 @@
     <header class="bg-no-repeat atypic-header bg-cover bg-center" style="background-image: url(https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQyMjAtcGRmYW1vdXNwYWludGluZ2V0YzA4MzAwOS1pbWFnZV8zLmpwZw.jpg)">
         <div class="h-full w-full">
                 <div class="h-full w-full bg-black/75 rounded-bl-full rounded-br-full flex flex-col items-center justify-evenly">
-                    <div class="w-40 border-b-4 border-b-yellow-500 pb-5">
+                    <div class="w-40">
                             <?php the_custom_logo(); ?>
                     </div>
-                    <a href="<?php echo home_url() ?>"><h1 class="text-6xl"><?php echo strtoupper(get_bloginfo('name')); ?></h1></a>
+                    <a href="<?php echo home_url() ?>"><h1 class="text-6xl underline underline-offset-[6px] decoration-yellow-500 decoration-4"><?php echo get_bloginfo('name'); ?></h1></a>
                     <?php
                     wp_nav_menu(array(
                         "theme_location" => "main_menu",
