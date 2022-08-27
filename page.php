6f463ed6e2774746ea39b7ecd9acbadb17fbf921
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-08-27 15:00:09 +0200

    Applying common wordpress css to page content

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
     <div class="atypic-main-left-laurel self-center w-0 sm:visible md:w-1/5">
         <?php get_template_part('assets/svg/laurel', 'single-left.svg') ?>
     </div>
-    <div class="p-5 md:w-3/5"><?php the_content() ?></div>
+    <div class="atypic-page p-5 md:w-3/5"><?php the_content() ?></div>
     <div class="atypic-main-right-laurel self-center w-0 md:visible md:w-1/5">
         <?php get_template_part('assets/svg/laurel', 'single-right.svg') ?>
     </div>
