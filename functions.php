c3928217c1127232c7a937d2545785fe6fb88f16
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-09-07 19:06:48 +0200

    Avoiding naming collisions in functions.php thourgh namespacing

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 
-require_once(get_template_directory().'/class-tgm-plugin-activation.php');
+namespace AtypicTheme\Functions;
+
+require_once(get_template_directory() . '/class-tgm-plugin-activation.php');
 
 //http://tgmpluginactivation.com/
-function atypic_register_required_plugins(){
+//Plugins dependencies
+function atypic_register_required_plugins()
+{
     $plugins = array(
         array(
             'name' => 'wpDiscuz',
@@ -32,21 +36,24 @@ function atypic_register_required_plugins(){
 
     tgmpa($plugins, $config);
 }
+add_action('tgmpa_register', '\AtypicTheme\Functions\atypic_register_required_plugins');
 
-add_action('tgmpa_register', 'atypic_register_required_plugins');
 
+//Remove jquery in the frontend
 function change_default_jquery()
 {
-    if (site_url() !== 'http://testing.local') {
+    if (!is_admin()) {
         wp_dequeue_script('jquery');
         wp_deregister_script('jquery');
     }
 }
 
-add_filter( 'show_admin_bar', '__return_false' );
-add_filter('wp_enqueue_scripts', 'change_default_jquery', PHP_INT_MAX);
+add_filter('wp_enqueue_scripts', '\AtypicTheme\Functions\change_default_jquery', PHP_INT_MAX);
 
+//Hide admin bar in the frontend
+add_filter('show_admin_bar', '__return_false');
 
+//WordPress features and support
 function atypique_features()
 {
     register_nav_menus(array(
@@ -55,23 +62,22 @@ function atypique_features()
     ));
 
     add_theme_support('custom-logo');
-    add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'title-tag' );
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'atypique_features');
+add_action('after_setup_theme', '\AtypicTheme\Functions\atypique_features');
 
+//Frontend styles
 function atypic_styles()
 {
-    //wp_enqueue_style('wp-block-library-css');
-    wp_enqueue_style( 'dashicons' );
+    //Loaded from top to bottom in HTML too
+    wp_enqueue_style('dashicons');
+    wp_enqueue_style('tailwind_styles', get_theme_file_uri('/style.css'));
+    wp_enqueue_style('index_css', get_theme_file_uri('/build/css/index.css'));
     wp_enqueue_style('header_css', get_theme_file_uri('/build/css/header.css'));
     wp_enqueue_style('footer_css', get_theme_file_uri('/build/css/footer.css'));
     wp_enqueue_style('home_css', get_theme_file_uri('/build/css/home.css'));
-    wp_enqueue_style('main_css', get_theme_file_uri('/build/css/main.css'));
-    wp_enqueue_style('index_css', get_theme_file_uri('/build/css/index.css'));
-    wp_enqueue_style('tailwind_styles', get_theme_file_uri('/style.css'));
-    //wp_enqueue_style('single_css', get_theme_file_uri('/build/css/single.css'));
 }
 
-add_action('wp_enqueue_scripts', 'atypic_styles');
+add_action('wp_enqueue_scripts', '\AtypicTheme\Functions\atypic_styles');
