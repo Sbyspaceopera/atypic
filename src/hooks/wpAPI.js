68e66b4f18e3fc629b367ce66788bb0798ff8bfd
Sebastien Corbisier <70596078+Sbyspaceopera@users.noreply.github.com>
2022-09-17 19:02:36 +0200

    Changing workplace, forgotten file

--- src/hooks/wpAPI.js
+++ src/hooks/wpAPI.js
@@ -5,11 +5,17 @@ import { apiFetch } from '@wordpress/api-fetch';
  * Query the WordPress REST API or custom routes.
  * @param {*} props 
  */
-function wpAPI({ body,method ="GET",apiSlugAndVersion = "wp/v2" }){
-    const rootURL = location.href + location.hostname + "/wp-json/wp/v2";
+function useWordPressAPI({params, fields}){
+    const [response, setResponse] = useEffect();
+    
+    const rootURL = location.href + location.hostname + "/wp-json/wp/v2/";
     apiFetch.use(apiFetch.createRootURLMiddleware(rootURL));
 
-    
+    useEffect(() =>{
+
+    }, [])
+
+
 
 
 
