ae494c31ed09f153336b52c3b52c106398f85dc0
Sbyspaceopera <sebastiencorbisiermurumba@gmail.com>
2023-02-02 11:08:54 +0100

    Add Webpack loader for the 3D model

--- atypic.webpack.config.js
+++ atypic.webpack.config.js
@@ -30,6 +30,19 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(glb|gltf)$/,
+        use:
+        [
+            {
+                loader: 'file-loader',
+                options:
+                {
+                    outputPath: 'assets/models/'
+                }
+            }
+        ]
+    },
     ],
   },
   stats: {
